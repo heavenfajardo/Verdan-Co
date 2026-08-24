78714b0ffd08de4e7101a7201cd057940a9114df
Heaven Fajardo <benfajardo380@gmail.com>
2026-08-24 11:22:23 +0800

    Add Aiven cloud database connection and CA certificate

--- db.php
+++ db.php
@@ -1,19 +1,22 @@
 <?php
-$host = 'localhost';
-$db   = 'verdant_db';
-$user = 'root';
-$pass = ''; 
-$charset = 'utf8mb4';
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
+$host = "mysql-3a335e21-heaven-portfolio.d.aivencloud.com";
+$username = "avnadmin";
+$password = "AVNS_ZH3Ra5dOJLcYbjDR6j-";
+$dbname = "defaultdb";
+$port = "14870";
+$caCert = __DIR__ . "/ca.pem"; // Make sure your ca.pem file is in the same folder
 
 try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
+    $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
+    
+    $options = [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::MYSQL_ATTR_SSL_CA       => $caCert,
+    ];
+
+    $pdo = new PDO($dsn, $username, $password, $options);
+
 } catch (\PDOException $e) {
     throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
